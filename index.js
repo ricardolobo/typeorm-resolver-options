ea96dc94e72946c0a2b28c23aaf2554b75c5dee8
Ricardo Lobo <ricardolobo@audienciazero.org>
2020-07-20 22:44:47 +0100

    adds settings using env vars

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 const typeorm = require('typeorm')
 
+/** reads configs from environment variables, if not set, use defaults*/
+function getConfigs() {
+    return {
+	pageSize: process.env.TRO_DEFAULT_PAGE_SIZE || 25,
+	searchField: process.env.TRO_DEFAULT_SEARCH_FIELD || 'name',
+	orderArg: process.env.TRO_ORDER_ARG || 'order',
+	searchArg: process.env.TRO_SEARCH_ARG || 'search',
+	pageArg: process.env.TRO_PAGE_ARG || 'page',
+	pageSizeArg: process.env.TRO_PAGE_SIZE_ARG || 'pageSize',
+    }
+}
+
 /** creates typeorm options object from graphql resolver args and info*/
 function buildOptions(args) {
-    // settings
-    let specialArgs = ['search', 'page', 'pageSize', 'order']
-    let searchField = 'description'
 
-    // pagination
-    let pageSize = args && args.pageSize || 25
+    const configs = getConfigs()
+
+    const {searchArg, pageArg, pageSizeArg, orderArg, searchField, pageSize} = configs
+
+    let reservedArgs = [
+	searchArg,
+	pageArg,
+	pageSizeArg,
+	orderArg
+    ]
+
+    let _pageSize = args && args.pageSize || pageSize
     let page = args && args.page || 1
 
-    // search & filter
     let where = {}
 
-    // search
-    if (args.search) {
-        const search = '%' + args.search + '%';
+    if (args[searchArg]) {
+        const search = '%' + args[searchArg] + '%';
         where[searchField] = typeorm.Raw(alias => `${alias} ILIKE '${search}' `)
     }
 
-    // ordering
     let order = { id: 'ASC' }
 
-    if (args.order) {
+    if (args[orderArg]) {
         order = { [args.order.field]: args.order.direction }
     }
 
     let options = {
         order,
         where,
-        skip: (page - 1) * pageSize,
-        take: pageSize
+        skip: (page - 1) * _pageSize,
+        take: _pageSize
     }
 
     return options
